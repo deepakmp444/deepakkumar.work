d00c61b4e296eed374032970cc35bff7bdd44eed
deepakmp444 <deepakmp444@gmail.com>
2022-10-29 23:38:34 +0530

    experience added

--- src/page/Experience.jsx
+++ src/page/Experience.jsx
@@ -7,9 +7,42 @@ function Experience() {
       animate={{ opacity: 1, nsition: { duration: 1 } }}
       exit={{ opacity: 0, nsition: { duration: 1 } }}
       variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
-      className="text-white"
+      className="text-white container p-5"
     >
-      Experience
+      <h1 className="addFont fontWeight900 heading-color mt-5">
+        Paktolus Solution
+      </h1>
+
+      <div className="addFont fontWeight500 heading-color">
+        <span>Full Time</span> | <span>1 Yr 11 Months</span>
+      </div>
+      <div className="addFont fontWeight500 heading-color">
+        <span className="fontWeight200">Mumbai, Maharashtra, India</span>{" "}
+        <span>
+          {" "}
+          <kbd>Remote</kbd>{" "}
+        </span>
+      </div>
+      <section className="addFont mt-3">
+        <div className="vl"></div>
+        <div style={{ marginTop: "-155px", marginLeft: "20px" }}>
+          <h5 className="heading-color">AR VR Architect </h5>
+          <p className="sub-heading-color fontWeight200">Oct 2021 - Aug 2022</p>
+        </div>
+        <div style={{ marginTop: "50px", marginLeft: "20px" }}>
+          <h5 className="heading-color"> Software Engineer VR/XR</h5>
+          <p className="sub-heading-color fontWeight200">Oct 2021 - Oct 2021</p>
+        </div>
+      </section>
+      <div
+        className="pointCSS"
+        style={{ marginTop: "-70px", marginLeft: "-7px" }}
+      ></div>
+
+      <div
+        className="pointCSS"
+        style={{ marginTop: "-120px", marginLeft: "-7px" }}
+      ></div>
     </motion.div>
   );
 }
